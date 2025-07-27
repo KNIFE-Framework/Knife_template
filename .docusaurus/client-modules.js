15241f9820772281fe7c7bab065beb22d7b89b98
roman kazicka <roman.kazicka@systemthinking.xyz>
2025-07-27 20:00:20 +0200

    doplnene .git subory z CAA template

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/romankazicka/Documents/800-CAS-ContextAwareApproach/03-KNIFE-Framework/knife_template_new/knife_template_v1/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/romankazicka/Documents/800-CAS-ContextAwareApproach/03-KNIFE-Framework/knife_template_new/knife_template_v1/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/romankazicka/Documents/800-CAS-ContextAwareApproach/03-KNIFE-Framework/knife_template_new/knife_template_v1/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/romankazicka/Documents/800-CAS-ContextAwareApproach/03-KNIFE-Framework/knife_template_new/knife_template_v1/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/romankazicka/Documents/800-CAS-ContextAwareApproach/03-KNIFE-Framework/knife_template_new/knife_template_v1/src/css/custom.css"),
+];
